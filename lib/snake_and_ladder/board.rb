7ee48fbcc1ce99117f6aa65c46bb00786aae595d
manuraj17 <manu.elayath@gmail.com>
2016-07-28 18:02:18 +0000

    Added position validation for snake and ladder

--- lib/snake_and_ladder/board.rb
+++ lib/snake_and_ladder/board.rb
@@ -14,12 +14,21 @@ class Board
       if s['head'] < ['tail']
         abort 'Error, snake head position must be greater than tail'
       end
+      # Checking if positions are valid for the board
+      if !s['head'].between?(0,@total_cells) || !s['tail'].between?(0, @total_cells)
+        abort 'Erro, invalid position'
+      end
+
       snake = Cell.new('snake', s['head'], s['tail'])
       @cells[s['cell']] = snake
     end
 
     # Initialising all the ladders
     config['ladders'].each do |l|
+      # Checking if positions are valid for the board
+      if !l['start'].between?(0, @total_cells) || !l['end'].between?(0, @total_cells)
+        abort 'Erro, invalid position'
+      end
       ladder = Cell.new('ladder', l['start'], l['end'])
       @cells[l['cell']] = ladder
     end
